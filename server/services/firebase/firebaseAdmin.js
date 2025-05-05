d08295987685ad59f2d6bc9e609c6ee15009d9c1
ttaminh03 <ttaminh03@gmail.com>
2025-05-05 12:31:08 +0700

    web

--- server/services/firebase/firebaseAdmin.js
+++ server/services/firebase/firebaseAdmin.js
@@ -1,8 +1,12 @@
 import admin from "firebase-admin";
 import dotenv from "dotenv";
+import { readFile } from "fs/promises";
+
 dotenv.config();
 
-import serviceAccount from "./json/firebase-service.json" assert { type: "json" };
+const serviceAccount = JSON.parse(
+  await readFile(new URL("./json/firebase-service.json", import.meta.url), "utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
